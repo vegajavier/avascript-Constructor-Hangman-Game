a65410f00c595acfa93ea8cbea4205128980c974
vegajavier <vegajavier86@gmail.com>
2018-10-19 00:20:37 -0500

    boxen api, figlet api, working code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log ("Hello!")
+//console.log ("Hello!")
 let Word = require("./word.js");
 let inquire = require("inquirer");
 /// npm i boxen use this for correct answers
@@ -7,6 +7,7 @@ const boxen = require("boxen");
 
 //looking at other code to use clc
 let clc = require("cli-color");
+var figlet = require('figlet');
 
 let incorrect = clc.red.bold;
 let correct = clc.green.bold;
@@ -24,6 +25,33 @@ let userGuesses = "";
 
 let lettersGuessed = "";
 let lettersGuessedList = [];
+let underScores = 0;
 
+figlet("QB Hangman", function(err,data ){
+    if (err){
+        //console.log("not working")
+        //n Firefox, these function behave quite differently: log only prints out a toString representation, whereas dir prints out a navigable tree. STACKOVERFLOW..
+
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+    //Welcome screen text. --- reference
+    console.log(gameTextColor("Welcome to the QB Hangman Game!"));
+    console.log(gameTextColor("Name that Legendary NFL QB"));
+
+    let playGame =
+    "   Instructions" + "\n"+
+    "------------------" + "\n"+
+    "Enter any letter when prompted to fill in the blanks and solve the word."+ "\n"+
+    "The correct letter you guessed appears in the word." + "\n" +
+    "The incorrect letter you guessed does not appear in the word." + "\n" +
+    "_________________________________________________________________________"
+    console.log(playGame);
+
+    startGame();
+});
+
+function startGame()
 
 
